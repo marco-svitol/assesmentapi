b05addc3f1022b89d9c97de2731798ed692b19cc
Marco <62987024+marco-svitol@users.noreply.github.com>
2022-02-05 17:29:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,3 @@
-sudo apt-get update
-sudo apt install docker
-
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
-
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-minikube start
 
 docker build . -t tikoapi:1.0
 docker pull redis
@@ -16,4 +6,5 @@ minikube image load redis:latest
 minikube image load tikoapi:1.0
 
 kubectl apply -f ./k8s/ns.yaml
-Kubectl apply -f k8s
+kubectl apply -f k8s
+kubectl expose deployment tikoapi-deploy -n tikoapi --type=NodePort
